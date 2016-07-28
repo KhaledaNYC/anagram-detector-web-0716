982ab0b54c5c5effbe2aa0d6b6a71fdf2e1b6325
Khaleda Khan <khaleda.khan87@gmail.com>
2016-07-28 13:30:25 +0000

    checkout select vs collect

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  def match(arr)
+    in_word = word.split("").sort.join("")
+    arr.select do |wordx|
+      an_word = wordx.split("").sort.join("")
+        if an_word == in_word
+          wordx
+        end
+      end
+  end
+end
